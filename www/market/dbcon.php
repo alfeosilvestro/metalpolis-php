4ae0d55ca85b53de837548ee6401e525c533c8d0
Galles <galles.cs@gmail.com>
2017-12-23 11:21:27 +0800

    db username is changed

--- www/market/dbcon.php
+++ www/market/dbcon.php
@@ -1,9 +1,9 @@
 <?php
     $database = "metalpolis_database";  // the name of the database.
     $server = "localhost";  // server to connect to.
-    $db_user = "root";  // mysql username to access the database with.
-    $db_pass = "";  // mysql password to access the database with.
-    
+    $db_user = "metal_dbuser";  // mysql username to access the database with.
+    $db_pass = "vackertech2018";  // mysql password to access the database with.
+
 	$conn = mysqli_connect($server, $db_user, $db_pass ,$database);
    if (mysqli_connect_errno())
 	{
