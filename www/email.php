56493ba16f0d7e5e14aac430394326f5c9ea53f8
Galles <galles.cs@gmail.com>
2018-01-06 13:37:41 +0800

    update contact mail

--- www/email.php
+++ www/email.php
@@ -1,45 +1,69 @@
 <?php
 if($_POST){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+//error_reporting(E_ALL);
+  error_reporting(E_STRICT);
+
+  date_default_timezone_set('America/Toronto');
+
+  require_once('class.phpmailer.php');
+  //include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+
+  $from_address = $_POST['email'];
+  $from_name = $_POST['name'];
+  $to_address = "info@metalpolis.com";
+  $to_name = "Info";
+  $subject = "Contact Us";
+  $message = $_POST['message'];
+  $smtp_host = "127.0.0.1";
+  $smtp_port = 25;
+  // $smtp_username = "info@metalpolis.com";
+  // $smtp_password = "12345678";
+  $smtp_username = "";
+  $smtp_password = "";
+  $smtp_debug = 2;
+
+  $mail             = new PHPMailer();
+
+  //$message             = file_get_contents('contents.html');
+  //$message             = eregi_replace("[\]",'',$message);
+  echo "Start SMTP Client";
+  $mail->IsSMTP(); // telling the class to use SMTP
+  $mail->Host       = $smtp_host; // SMTP server
+  $mail->SMTPDebug  = $smtp_debug;                     // enables SMTP debug information (for testing)
+                                             // 1 = errors and messages
+                                             // 2 = messages only
+  $mail->SMTPAuth   = false;                  // enable SMTP authentication
+  $mail->Port       = $smtp_port;                    // set the SMTP port for the GMAIL server
+  //$mail->Username   = $smtp_username;       // SMTP account username
+  //$mail->Password   = $smtp_password;        // SMTP account password
+
+  $mail->SetFrom($from_address, $from_name);
+
+  $mail->AddReplyTo($from_address, $from_name);
+
+  $mail->Subject    = $subject;
+
+  $mail->AltBody    = $message; // optional, comment out and test
+
+  $mail->MsgHTML($message);
+
+  $mail->AddAddress($to_address, $to_name);
+
+  //$mail->AddAttachment("images/phpmailer.gif");      // attachment
+  //$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
 
 
-/* Email Detials */
-  $mail_to = "info@metalpolis.com";
-  $from_mail = $email;
-  $from_name = $name;
-  $reply_to = $from_mail;
-  $subject = "Contact";
 
-/* Set the email header */
-  // Generate a boundary
-  $boundary = md5(uniqid(time()));
-
-  // Email header
-  $header = "From: ".$from_name." <".$from_mail.">".PHP_EOL;
-  $header .= "Reply-To: ".$reply_to.PHP_EOL;
-  $header .= "MIME-Version: 1.0".PHP_EOL;
-
-  // Multipart wraps the Email Content and Attachment
-  $header .= "Content-Type: multipart/mixed; boundary=\"".$boundary."\"".PHP_EOL;
-  $header .= "This is a multi-part message in MIME format.".PHP_EOL;
-  $header .= "--".$boundary.PHP_EOL;
-
-  // Email content
-  // Content-type can be text/plain or text/html
-  $header .= "Content-type:text/plain; charset=iso-8859-1".PHP_EOL;
-  $header .= "Content-Transfer-Encoding: 7bit".PHP_EOL;
-  $header .= $message.PHP_EOL;
-  $header .= "--".$boundary.PHP_EOL;
-
-
-
-  // Send email
-  if (mail($mail_to, $subject, $message, $header)) {
-    echo "Sent";
-  } else {
-    echo "Error";
+  try {
+    echo "Begin send";
+    if(!$mail->Send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+      echo "Sent";
+    }
+  }
+  catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
   }
 }
 ?>
