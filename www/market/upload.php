1e6612e5f25d622a7603c60f86d78a6d5294c204
alfeosilvestro <alfeo.silvestro@live.com>
2018-02-01 18:14:22 +0800

    added more debugging parameters to upload logic

--- www/market/upload.php
+++ www/market/upload.php
@@ -7,24 +7,29 @@ header('Content-Type: application/json');
 				echo json_encode(array('status' => 'Error', 'message' =>$date .'_'. $_FILES['file']['error']));
     }
     else {
-				//$target = $_SERVER['DOCUMENT_ROOT'] . '/metalpolis_git/www/market/attachment/';				
-				$target = $_SERVER['DOCUMENT_ROOT'] . "/market/attachment/";				
+				//$target = $_SERVER['DOCUMENT_ROOT'] . '/metalpolis_git/www/market/attachment/';		
+				$doc_root = $_SERVER['DOCUMENT_ROOT'];		
+				$target = $doc_root . "/market/attachment/";				
 
+				//// Folder check logic - Return error if upload folder not exists
 				if (!file_exists($target)) {
-					echo json_encode(array('status' => 'Director Error', 'path' => $target, 'error' => 'folder not exists'));
+					echo json_encode(array('status' => 'Directory Error', 'doc root' => $doc_root, 'upload path' => $target, 'error' => 'Attachment folder not exists'));
 						return;
 				}
 
+				//// Catch error - return error if file upload has permission or other issues
 				try{
-
+					//// Logic - Upload file from memory to target file
 					move_uploaded_file($_FILES['file']['tmp_name'], $target.$date .'_'. $_FILES['file']['name']);
 				
 				}catch(Exception $e){
-					echo json_encode(array('status' => 'Upload Fail', 'message' =>$date .'_'. $_FILES['file']['name'], 'path' => $target, 'error' => $e->getMessage()));
+					//// Result - Fail
+					echo json_encode(array('status' => 'Upload Fail', 'message' =>$date .'_'. $_FILES['file']['name'], 'doc root' => $doc_root, 'upload path' => $target, 'error' => $e->getMessage()));
 					return;
 				}
 
-				echo json_encode(array('status' => 'Success', 'message' =>$date .'_'. $_FILES['file']['name'], 'path' => $target));
+				//// Result - Success
+				echo json_encode(array('status' => 'Success', 'message' =>$date .'_'. $_FILES['file']['name'], 'doc root' => $doc_root, 'upload path' => $target));
     }
 	}
 ?>
