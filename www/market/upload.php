020f598802e326b225478cfbf43bad4bffa1bb51
alfeosilvestro <alfeo.silvestro@live.com>
2018-02-01 11:59:30 +0800

    file upload bug fixes for docker

--- www/market/upload.php
+++ www/market/upload.php
@@ -7,9 +7,24 @@ header('Content-Type: application/json');
 				echo json_encode(array('status' => 'Error', 'message' =>$date .'_'. $_FILES['file']['error']));
     }
     else {
-				$target = $_SERVER['DOCUMENT_ROOT'] . '/metalpolis_git/www/market/attachment/';
-        move_uploaded_file($_FILES['file']['tmp_name'], $target.$date .'_'. $_FILES['file']['name']);
-				echo json_encode(array('status' => 'Success', 'message' =>$date .'_'. $_FILES['file']['name']));
+				//$target = $_SERVER['DOCUMENT_ROOT'] . '/metalpolis_git/www/market/attachment/';				
+				$target = $_SERVER['DOCUMENT_ROOT'] . "/market/attachment/";				
+
+				if (!file_exists($target)) {
+					echo json_encode(array('status' => 'Director Error', 'path' => $target, 'error' => 'folder not exists'));
+						return;
+				}
+
+				try{
+
+					move_uploaded_file($_FILES['file']['tmp_name'], $target.$date .'_'. $_FILES['file']['name']);
+				
+				}catch(Exception $e){
+					echo json_encode(array('status' => 'Upload Fail', 'message' =>$date .'_'. $_FILES['file']['name'], 'path' => $target, 'error' => $e->getMessage()));
+					return;
+				}
+
+				echo json_encode(array('status' => 'Success', 'message' =>$date .'_'. $_FILES['file']['name'], 'path' => $target));
     }
 	}
 ?>
